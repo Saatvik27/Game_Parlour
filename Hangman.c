58698284a8b57d5aee11a0d1a4de0a7445ea430c
Saatvik27 <111062378+Saatvik27@users.noreply.github.com>
2022-12-10 22:01:32 +0530

    Update Hangman.c

--- Hangman.c
+++ Hangman.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     char word[100],hint[100];
     printf("Enter a word : ");
@@ -6,29 +7,46 @@ int main(){
     printf("Enter a hint :");
     gets(hint);
     int size=0;
-    char guess[6];
-    for(int i=0;i!='\0';i++){
+    char guess[100];
+    for(int i=0;word[i]!='\0';i++){
         size+=1;
     }
-    for(int j=0;j<size;j++){
-        printf("_ ");
-    }
     printf("You will have 6 guesses till hangman dies .\n" );
-    for(int i=0;i<6;i++){
+    printf("Your hint is : %s\n",hint);
+    for(int j=0;j<size;j++){
+        printf("_");
+    }
+    printf("\n");
+    int x;
+    if(size>6){
+        x=size;
+    }
+    else{
+        x=6;
+    }
+    char g[100];
+    for(int i=0;i<size;i++){
+        g[i]='_';
+    }
+    int count=0;
+    for(int i=0;i<x;i++){
+        if(strcmp(g,word)==0){
+            break;
+        }
+        else{
         printf("Enter Guess : ");
-        scanf("%c",&guess[i]);
+        scanf("%s",&guess[i]);
         for(int j=0;word[j]!='\0';j++){
-            for(int k=0;k<i;k++){
-                if(word[j]==guess[k]){
-                    printf("%c",guess[k]);
-                }
-            }
-             else{
-                printf("_");
+            if(guess[i]==word[j]){
+                g[j]=guess[i];
             }
         }
+        printf("%s",g);
         printf("\n");
+        }
     }
+
     return 0;
 }
+
 
