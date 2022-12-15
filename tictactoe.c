73c4ccec3d3a6cd7da69380df1e1cc1f4ae49980
Saatvik27 <111062378+Saatvik27@users.noreply.github.com>
2022-12-15 22:12:19 +0530

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -21,6 +21,23 @@ int wincase(char x, char m[3][3]){
     }
     return(flag);
 }
+int filled(char m[3][3]){
+    int flag,count=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(m[i][j]!='_'){
+                count++;
+            }
+        }
+    }
+    if(count==9){
+        flag=1;
+    }
+    else{
+        flag=0;
+    }
+    return flag;
+}
 int main(){
     printf("TIC TAC TOE\n");
     char p1,p2;
@@ -46,100 +63,117 @@ int main(){
     }
     printf("\n");
 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            printf("P1 : Enter Box Number - ");
-            scanf("%d",&num);
-            if(num==1 && matrix[0][0]=='_'){
-                matrix[0][0]=p1;
+    for(int i=0;i<100;i++){
+        for(int j=0;j<100;j++){
+            if((wincase(p1,matrix)==1 || wincase(p2,matrix)==1) &&  filled(matrix)==1){
+                break;
             }
-            else if(num==2 && matrix[0][1]=='_'){
-                matrix[0][1]=p1;
-            }
-            else if(num==3 && matrix[0][2]=='_'){
-                matrix[0][2]=p1;
-            }
-            else if(num==4 && matrix[1][0]=='_'){
-                matrix[1][0]=p1;
-            }
-            else if(num==5 && matrix[1][1]=='_'){
-                matrix[1][1]=p1;
-            }
-            else if(num==6 && matrix[1][2]=='_'){
-                matrix[1][2]=p1;
-            }
-            else if(num==7 && matrix[2][0]=='_'){
-                matrix[2][0]=p1;
-            }
-            else if(num==8 && matrix[2][1]=='_'){
-                matrix[2][1]=p1;
-            }
-            else if(num==9 && matrix[2][2]=='_'){
-                matrix[2][2]=p1;
-            }
-            printf("\n");
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    if(j==0 || j==1){
-                        printf(" %c |",matrix[i][j]);
+            else if((wincase(p1,matrix)==0 && wincase(p2,matrix)==0) && filled(matrix)==0){
+                printf("P1 : Enter Box Number - ");
+                scanf("%d",&num);
+                if(num==1 && matrix[0][0]=='_'){
+                    matrix[0][0]=p1;
+                }
+                else if(num==2 && matrix[0][1]=='_'){
+                    matrix[0][1]=p1;
+                }
+                else if(num==3 && matrix[0][2]=='_'){
+                    matrix[0][2]=p1;
+                }
+                else if(num==4 && matrix[1][0]=='_'){
+                    matrix[1][0]=p1;
+                }
+                else if(num==5 && matrix[1][1]=='_'){
+                    matrix[1][1]=p1;
+                }
+                else if(num==6 && matrix[1][2]=='_'){
+                    matrix[1][2]=p1;
+                }
+                else if(num==7 && matrix[2][0]=='_'){
+                    matrix[2][0]=p1;
+                }
+                else if(num==8 && matrix[2][1]=='_'){
+                    matrix[2][1]=p1;
+                }
+                else if(num==9 && matrix[2][2]=='_'){
+                    matrix[2][2]=p1;
+                }
+                printf("\n");
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                        if(j==0 || j==1){
+                            printf(" %c |",matrix[i][j]);
+                        }
+                        else{
+                            printf(" %c ",matrix[i][j]);
+                        }
+                    }
+                    if(i==0 || i==1){
+                        printf("\n---|---|---\n");
                     }
                     else{
-                        printf(" %c ",matrix[i][j]);
+                        printf("\n");
                     }
                 }
-                if(i==0 || i==1){
-                    printf("\n---|---|---\n");
-                }
-                else{
-                    printf("\n");
-                }
-            }
-            printf("P2 : Enter Box Number - ");
-            scanf("%d",&num);
-            if(num==1 && matrix[0][0]=='_'){
+                if(wincase(p1,matrix)!=1 && filled(matrix)==0){
+                printf("P2 : Enter Box Number - ");
+                scanf("%d",&num);
+                if(num==1 && matrix[0][0]=='_'){
                 matrix[0][0]=p2;
-            }
-            else if(num==2 && matrix[0][1]=='_'){
+                }
+                else if(num==2 && matrix[0][1]=='_'){
                 matrix[0][1]=p2;
-            }
-            else if(num==3 && matrix[0][2]=='_'){
+                }
+                else if(num==3 && matrix[0][2]=='_'){
                 matrix[0][2]=p2;
-            }
-            else if(num==4 && matrix[1][0]=='_'){
+                }
+                else if(num==4 && matrix[1][0]=='_'){
                 matrix[1][0]=p2;
-            }
-            else if(num==5 && matrix[1][1]=='_'){
+                }
+                else if(num==5 && matrix[1][1]=='_'){
                 matrix[1][1]=p2;
-            }
-            else if(num==6 && matrix[1][2]=='_'){
+                }
+                else if(num==6 && matrix[1][2]=='_'){
                 matrix[1][2]=p2;
-            }
-            else if(num==7 && matrix[2][0]=='_'){
+                }
+                else if(num==7 && matrix[2][0]=='_'){
                 matrix[2][0]=p2;
-            }
-            else if(num==8 && matrix[2][1]=='_'){
+                }
+                else if(num==8 && matrix[2][1]=='_'){
                 matrix[2][1]=p2;
-            }
-            else if(num==9 && matrix[2][2]=='_'){
-                matrix[2][2]=p2;
-            }
-            printf("\n");
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    if(j==0 || j==1){
-                        printf(" %c |",matrix[i][j]);
+                }
+                else if(num==9 && matrix[2][2]=='_'){
+                    matrix[2][2]=p2;
+                }
+                printf("\n");
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                        if(j==0 || j==1){
+                            printf(" %c |",matrix[i][j]);
+                        }
+                        else{
+                            printf(" %c ",matrix[i][j]);
+                        }
+                    }
+                    if(i==0 || i==1){
+                        printf("\n---|---|---\n");
                     }
                     else{
-                        printf(" %c ",matrix[i][j]);
+                        printf("\n");
+                        }
                     }
-                }
-                if(i==0 || i==1){
-                    printf("\n---|---|---\n");
-                }
-                else{
-                    printf("\n");
                 }
             }
         }
     }
-    return 0;}
+    if(wincase(p1,matrix)==1){
+        printf("P1 Wins");
+    }
+    else if(wincase(p2,matrix)==1){
+        printf("P2 Wins");
+    }
+    else if(wincase(p1,matrix)==0 && wincase(p2,matrix)==0 && filled(matrix)==1){
+        printf("Draw");
+    }
+    return 0;
+    }
